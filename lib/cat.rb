d25eaf63201165de47c97dc546054c49b14c28c7
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 20:04:14 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
 attr_reader :name
-attr_accessor :owner, :moodx
+attr_accessor :owner, :mood
 
 def initialize(name, owner)
   @name = name
   @owner = owner
+  @mood = "nervous"
 end
 
 
