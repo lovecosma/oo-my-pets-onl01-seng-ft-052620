67cb9a57df00209b9563ec0a604b381797f03279
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 19:56:24 +0000

    Automatically backed up by Learn

---
+++ lib/owner.rb
@@ -0,0 +1,24 @@
+class Owner
+  attr_accessor
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+  end
+
+def say_species
+  "I am a " + species + "."
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @all.count
+end
+
+end
